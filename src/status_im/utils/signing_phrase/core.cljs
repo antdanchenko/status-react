891b1e175a67be1686dffb54bc670a342839e803
Goran Jovic <goranjovic@gmail.com>
2017-08-28 09:59:33 +0200

    added a brief verbal descripion

--- src/status_im/utils/signing_phrase/core.cljs
+++ src/status_im/utils/signing_phrase/core.cljs
@@ -3,6 +3,12 @@
             [status-im.utils.signing-phrase.dictionaries.ru :as ru]
             [clojure.string :as string]))
 
+;`signing-phrase` is a language-specific combination of three 4-lettered words
+; used as an anti-phishing device.
+;
+; Currently only English is supported (as the default language),
+; for more details see #https://github.com/status-im/status-react/issues/1679
+
 (def dictionaries
   {:en en/dictionary
    :ru ru/dictionary})
